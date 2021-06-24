0df2b2c4444890a70bd397fb1a392e5ffec820d1
George Papagapitos <george.g.papagapitos@gmail.com>
2021-06-24 12:21:21 -0500

    cypress testing set up

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -21,11 +21,11 @@ const LoginForm = ({ login }) => {
     <form onSubmit={handleLogin}>
       <div>
         username
-        <input type="text" value={username} name="Username" onChange={({ target }) => setUsername(target.value)} />
+        <input id="username" type="text" value={username} name="Username" onChange={({ target }) => setUsername(target.value)} />
       </div>
       <div>
         password
-        <input type="password" value={password} name="Password" onChange={({ target }) => setPassword(target.value)} />
+        <input id="password" type="password" value={password} name="Password" onChange={({ target }) => setPassword(target.value)} />
       </div>
       <button>login</button>
     </form>
